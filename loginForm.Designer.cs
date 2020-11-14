482e84220876dcb1733591c1c6be6606f2cf3f3c
Samuel Sloboda <sam.sloboda@gmail.com>
2020-11-14 22:19:46 +0100

    Add project files.

---
+++ loginForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Work_Log_Project
+{
+    partial class loginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_login = new System.Windows.Forms.Button();
+            this.lb_userName = new System.Windows.Forms.Label();
+            this.lb_password = new System.Windows.Forms.Label();
+            this.tb_username = new System.Windows.Forms.TextBox();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.lb_incorrectMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bt_login
+            // 
+            this.bt_login.Location = new System.Drawing.Point(116, 198);
+            this.bt_login.Name = "bt_login";
+            this.bt_login.Size = new System.Drawing.Size(153, 49);
+            this.bt_login.TabIndex = 2;
+            this.bt_login.Text = "Login";
+            this.bt_login.UseVisualStyleBackColor = true;
+            this.bt_login.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lb_userName
+            // 
+            this.lb_userName.AutoSize = true;
+            this.lb_userName.Location = new System.Drawing.Point(36, 63);
+            this.lb_userName.Name = "lb_userName";
+            this.lb_userName.Size = new System.Drawing.Size(77, 17);
+            this.lb_userName.TabIndex = 1;
+            this.lb_userName.Text = "Username:";
+            // 
+            // lb_password
+            // 
+            this.lb_password.AutoSize = true;
+            this.lb_password.Location = new System.Drawing.Point(40, 120);
+            this.lb_password.Name = "lb_password";
+            this.lb_password.Size = new System.Drawing.Size(73, 17);
+            this.lb_password.TabIndex = 2;
+            this.lb_password.Text = "Password:";
+            // 
+            // tb_username
+            // 
+            this.tb_username.Location = new System.Drawing.Point(154, 60);
+            this.tb_username.MaxLength = 10;
+            this.tb_username.Name = "tb_username";
+            this.tb_username.Size = new System.Drawing.Size(175, 22);
+            this.tb_username.TabIndex = 0;
+            // 
+            // tb_password
+            // 
+            this.tb_password.Location = new System.Drawing.Point(154, 120);
+            this.tb_password.MaxLength = 10;
+            this.tb_password.Name = "tb_password";
+            this.tb_password.PasswordChar = '*';
+            this.tb_password.Size = new System.Drawing.Size(175, 22);
+            this.tb_password.TabIndex = 1;
+            // 
+            // lb_incorrectMessage
+            // 
+            this.lb_incorrectMessage.AutoSize = true;
+            this.lb_incorrectMessage.ForeColor = System.Drawing.Color.Red;
+            this.lb_incorrectMessage.Location = new System.Drawing.Point(82, 165);
+            this.lb_incorrectMessage.Name = "lb_incorrectMessage";
+            this.lb_incorrectMessage.Size = new System.Drawing.Size(214, 17);
+            this.lb_incorrectMessage.TabIndex = 5;
+            this.lb_incorrectMessage.Text = "Incorrect username or password!";
+            // 
+            // loginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 259);
+            this.Controls.Add(this.lb_incorrectMessage);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.tb_username);
+            this.Controls.Add(this.lb_password);
+            this.Controls.Add(this.lb_userName);
+            this.Controls.Add(this.bt_login);
+            this.Name = "loginForm";
+            this.Text = "Login to Work Log";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_login;
+        private System.Windows.Forms.Label lb_userName;
+        private System.Windows.Forms.Label lb_password;
+        private System.Windows.Forms.TextBox tb_username;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.Label lb_incorrectMessage;
+    }
+}
+
