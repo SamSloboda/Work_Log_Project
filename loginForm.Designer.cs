501cdb3c3aba7c8fad6ad39e07a0f6a29aeea09c
Moe <Moe@shepherd.edu>
2020-12-02 15:43:51 -0500

    Added panels

--- loginForm.Designer.cs
+++ loginForm.Designer.cs
@@ -33,6 +33,18 @@
             this.lb_password = new System.Windows.Forms.Label();
             this.tb_username = new System.Windows.Forms.TextBox();
             this.tb_password = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
             this.SuspendLayout();
             // 
             // bt_login
@@ -40,7 +52,7 @@
             this.bt_login.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.bt_login.Font = new System.Drawing.Font("Trebuchet MS", 15.75F);
             this.bt_login.ForeColor = System.Drawing.Color.White;
-            this.bt_login.Location = new System.Drawing.Point(42, 393);
+            this.bt_login.Location = new System.Drawing.Point(3, 3);
             this.bt_login.Name = "bt_login";
             this.bt_login.Size = new System.Drawing.Size(158, 38);
             this.bt_login.TabIndex = 2;
@@ -53,7 +65,7 @@
             this.lb_userName.AutoSize = true;
             this.lb_userName.Font = new System.Drawing.Font("Trebuchet MS", 15.75F);
             this.lb_userName.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.lb_userName.Location = new System.Drawing.Point(37, 51);
+            this.lb_userName.Location = new System.Drawing.Point(16, 120);
             this.lb_userName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lb_userName.Name = "lb_userName";
             this.lb_userName.Size = new System.Drawing.Size(112, 27);
@@ -65,7 +77,7 @@
             this.lb_password.AutoSize = true;
             this.lb_password.Font = new System.Drawing.Font("Trebuchet MS", 15.75F);
             this.lb_password.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.lb_password.Location = new System.Drawing.Point(37, 176);
+            this.lb_password.Location = new System.Drawing.Point(16, 218);
             this.lb_password.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lb_password.Name = "lb_password";
             this.lb_password.Size = new System.Drawing.Size(108, 27);
@@ -76,7 +88,7 @@
             // 
             this.tb_username.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.tb_username.Font = new System.Drawing.Font("Trebuchet MS", 20.25F);
-            this.tb_username.Location = new System.Drawing.Point(30, 81);
+            this.tb_username.Location = new System.Drawing.Point(35, 12);
             this.tb_username.MaxLength = 10;
             this.tb_username.Name = "tb_username";
             this.tb_username.Size = new System.Drawing.Size(348, 32);
@@ -86,12 +98,69 @@
             // 
             this.tb_password.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.tb_password.Font = new System.Drawing.Font("Trebuchet MS", 20.25F);
-            this.tb_password.Location = new System.Drawing.Point(30, 223);
+            this.tb_password.Location = new System.Drawing.Point(35, 12);
             this.tb_password.MaxLength = 10;
             this.tb_password.Name = "tb_password";
             this.tb_password.PasswordChar = '*';
             this.tb_password.Size = new System.Drawing.Size(348, 32);
             this.tb_password.TabIndex = 17;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Crimson;
+            this.panel1.Controls.Add(this.panel7);
+            this.panel1.Location = new System.Drawing.Point(0, 150);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(424, 54);
+            this.panel1.TabIndex = 18;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Crimson;
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Location = new System.Drawing.Point(0, 248);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(425, 54);
+            this.panel2.TabIndex = 19;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Crimson;
+            this.panel3.Controls.Add(this.panel5);
+            this.panel3.Location = new System.Drawing.Point(112, 393);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(170, 50);
+            this.panel3.TabIndex = 20;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(24)))), ((int)(((byte)(23)))));
+            this.panel7.Controls.Add(this.tb_username);
+            this.panel7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(86)))), ((int)(((byte)(83)))), ((int)(((byte)(80)))));
+            this.panel7.Location = new System.Drawing.Point(0, 3);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(425, 47);
+            this.panel7.TabIndex = 21;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(24)))), ((int)(((byte)(23)))));
+            this.panel4.Controls.Add(this.tb_password);
+            this.panel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(86)))), ((int)(((byte)(83)))), ((int)(((byte)(80)))));
+            this.panel4.Location = new System.Drawing.Point(0, 4);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(425, 47);
+            this.panel4.TabIndex = 22;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(35)))), ((int)(((byte)(33)))));
+            this.panel5.Controls.Add(this.bt_login);
+            this.panel5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(24)))), ((int)(((byte)(23)))));
+            this.panel5.Location = new System.Drawing.Point(3, 3);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(164, 44);
+            this.panel5.TabIndex = 21;
             // 
             // loginForm
             // 
@@ -99,16 +168,24 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(35)))), ((int)(((byte)(33)))));
             this.ClientSize = new System.Drawing.Size(412, 559);
-            this.Controls.Add(this.tb_password);
-            this.Controls.Add(this.tb_username);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.lb_password);
             this.Controls.Add(this.lb_userName);
-            this.Controls.Add(this.bt_login);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "loginForm";
             this.Text = "Login to Work Log";
             this.Load += new System.EventHandler(this.loginForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,6 +198,12 @@
         private System.Windows.Forms.Label lb_password;
         private System.Windows.Forms.TextBox tb_username;
         private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel5;
     }
 }
 
