f24a882a63015689c18c79f734c2d45f466508bc
Moe <Moe@shepherd.edu>
2020-12-06 08:43:38 -0500

    changed design and edited the Removeattendanceform

--- EmployeeRemoveAttendaceForm.Designer.cs
+++ EmployeeRemoveAttendaceForm.Designer.cs
@@ -31,24 +31,37 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.bt_removeAttendance = new System.Windows.Forms.Button();
             this.bt_exit = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(39, 18);
+            this.dataGridView1.Location = new System.Drawing.Point(29, 15);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(557, 384);
+            this.dataGridView1.Size = new System.Drawing.Size(418, 312);
             this.dataGridView1.TabIndex = 0;
             // 
             // bt_removeAttendance
             // 
-            this.bt_removeAttendance.Location = new System.Drawing.Point(39, 436);
+            this.bt_removeAttendance.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bt_removeAttendance.Font = new System.Drawing.Font("Trebuchet MS", 15.75F);
+            this.bt_removeAttendance.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.bt_removeAttendance.Location = new System.Drawing.Point(2, 2);
+            this.bt_removeAttendance.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bt_removeAttendance.Name = "bt_removeAttendance";
-            this.bt_removeAttendance.Size = new System.Drawing.Size(168, 23);
+            this.bt_removeAttendance.Size = new System.Drawing.Size(201, 40);
             this.bt_removeAttendance.TabIndex = 1;
             this.bt_removeAttendance.Text = "Remove Attendance";
             this.bt_removeAttendance.UseVisualStyleBackColor = true;
@@ -56,25 +69,75 @@
             // 
             // bt_exit
             // 
-            this.bt_exit.Location = new System.Drawing.Point(213, 436);
+            this.bt_exit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bt_exit.Font = new System.Drawing.Font("Trebuchet MS", 15.75F);
+            this.bt_exit.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.bt_exit.Location = new System.Drawing.Point(2, 2);
+            this.bt_exit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bt_exit.Name = "bt_exit";
-            this.bt_exit.Size = new System.Drawing.Size(168, 23);
+            this.bt_exit.Size = new System.Drawing.Size(160, 40);
             this.bt_exit.TabIndex = 2;
             this.bt_exit.Text = "Exit";
             this.bt_exit.UseVisualStyleBackColor = true;
             this.bt_exit.Click += new System.EventHandler(this.bt_exit_Click);
             // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Crimson;
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(24)))), ((int)(((byte)(23)))));
+            this.panel3.Location = new System.Drawing.Point(29, 361);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(212, 50);
+            this.panel3.TabIndex = 14;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(35)))), ((int)(((byte)(33)))));
+            this.panel4.Controls.Add(this.bt_removeAttendance);
+            this.panel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(24)))), ((int)(((byte)(23)))));
+            this.panel4.Location = new System.Drawing.Point(3, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(206, 44);
+            this.panel4.TabIndex = 14;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Crimson;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(24)))), ((int)(((byte)(23)))));
+            this.panel1.Location = new System.Drawing.Point(277, 361);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(170, 50);
+            this.panel1.TabIndex = 15;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(35)))), ((int)(((byte)(33)))));
+            this.panel2.Controls.Add(this.bt_exit);
+            this.panel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(24)))), ((int)(((byte)(23)))));
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(164, 44);
+            this.panel2.TabIndex = 14;
+            // 
             // EmployeeRemoveAttendaceForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(644, 492);
-            this.Controls.Add(this.bt_exit);
-            this.Controls.Add(this.bt_removeAttendance);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(35)))), ((int)(((byte)(33)))));
+            this.ClientSize = new System.Drawing.Size(483, 442);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "EmployeeRemoveAttendaceForm";
             this.Text = "Remove Attendance";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -84,5 +147,9 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button bt_removeAttendance;
         private System.Windows.Forms.Button bt_exit;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
